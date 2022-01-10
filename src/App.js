d522c3e76cb34b7a30925ba085004790b58f768e
pablo179 <cm.pablo.prog@gmail.com>
2022-01-10 13:32:46 -0600

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      ****** Div
+        1.- div con texto que no hace nada
+        2.- div con texto dinamico
+        3.- div con texto y estilos dinamicos
+        4.- div que desaparece
+        5.- div con funcion dinamica
+      </>
+      <>
+      ****** Button
+        1.- boton que no hace nada
+        2.- boton con texto dinamico
+        3.- boton con texto y estilos dinamicos
+        4.- boton que desaparece
+        5.- boton con funcion dinamica
+      </>
+      <>
+      ****** Input
+        1.- input que no hace nada
+        2.- input con texto dinamico
+        3.- input con texto y estilos dinamicos
+        4.- input que desaparece
+        5.- input con funcion dinamica
+      </>
+      <>
+      ****** Pokemon
+        1.- pokemon component (all examples)
+      </>
+      <>
+      ****** Message
+        1.- message component (all examples)
+      </>
+      <>
+      ****** Cards
+        1.- cards component (all examples)
+      </>
     </div>
   );
 }
