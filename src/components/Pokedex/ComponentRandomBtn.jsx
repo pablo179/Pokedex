72b3409921f6263f6af7b28aea41153064031bca
Emmanuelcazlo <Emmanuelcazlo@gmail.com>
2022-01-14 15:41:40 -0600

    New components to PokemonCard and new styles

---
+++ src/components/Pokedex/ComponentRandomBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentRandomBtn = ()=>{
+    return <button className="componentRandomBtn"> <img src="" className="componentRandomBtn--image"/> ¡Sorprendeme!</button>
+}
+
+export default ComponentRandomBtn;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Pokedex from './Pokedex'
+import Pokemon from './Pokemon'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App/>}/>
+        <Route exact path="/pokedex" element={<Pokedex/>}/>
+        <Route exact path="/pokedex/pokemon" element={<Pokemon/>}/>
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
