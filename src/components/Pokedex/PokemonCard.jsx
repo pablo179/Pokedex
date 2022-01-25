9ada3b31f0fb8284bb2dab694ea00eec07457ae9
Emmanuelcazlo <Emmanuelcazlo@gmail.com>
2022-01-24 18:18:26 -0600

    Number format

--- src/components/Pokedex/PokemonCard.jsx
+++ src/components/Pokedex/PokemonCard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { CardContainer, CardRow,PokemonNumber, TypeContainer,Type,PokemonImg } from "./styles";
 const PokemonCard = ({name,id,image,color,types}) =>{
     const pokemontype = types;
+    const realNumber = ()=>{ 
+        return id > 100 ? 'N° '+id : id > 10 ? 'N° 0'+id : 'N° 00'+id;
+    }
     return(
         <CardContainer key={id} color={color} >
             <CardRow>
