14d23db539cb3f3de8b5fb3bc63b90061d64fede
Emmanuel <33813095+Emmanuelcazlo@users.noreply.github.com>
2022-02-07 18:12:22 -0600

    Revert "Fixing errors"

--- src/components/Pokedex/PokemonCard.jsx
+++ src/components/Pokedex/PokemonCard.jsx
@@ -1,65 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { realNumber, loadDefault } from '../../utils';
-
-import {
-  CardContainer,
-  CardRow,
-  PokemonNumber,
-  TypeContainer,
-  Type,
-  PokemonImg,
-  PokemonRow,
-} from './styles';
-
-function PokemonCard({
-  name, id, image, color, types, openModal,
-}) {
-  const pokemontype = types;
-
-  return (
-    <CardContainer
-      key={id}
-      color={color}
-      onClick={() => openModal(
-        {
-          name,
-          id,
-          image,
-          color,
-          types,
-        },
-      )}
-    >
-      <CardRow>
-        {name}
-        <PokemonNumber color={color}>
-          {realNumber(id)}
-        </PokemonNumber>
-      </CardRow>
-      <PokemonRow>
-        <TypeContainer>
-          {
-             pokemontype.map((item) => <Type type={item.type.name}>{item.type.name}</Type>)
-          }
-        </TypeContainer>
-        <PokemonImg onError={(e) => { loadDefault(e, id); }} src={image} alt="Pokemon Image" />
-      </PokemonRow>
-    </CardContainer>
-  );
+import React from "react";
+import { CardContainer, CardRow,PokemonNumber, TypeContainer,Type,PokemonImg } from "./styles";
+const realNumber = (id)=>{ 
+    return id > 100 ? 'N° '+id : id > 10 ? 'N° 0'+id : 'N° 00'+id;
 }
-PokemonCard.defaulProps = {
-  name: 'PokemonName',
-  id: 'N° 000',
-  image: '',
-  color: '#000000',
-};
-PokemonCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-  types: PropTypes.arrayOf(PropTypes.string).isRequired,
-  openModal: PropTypes.func.isRequired,
-};
+const PokemonCard = ({name,id,image,color,types}) =>{
+    const pokemontype = types;
+    return(
+        <CardContainer key={id} color={color} >
+            <CardRow>
+                {name}
+                <PokemonNumber color={color}>{realNumber(id)} </PokemonNumber>
+            </CardRow>
+            <CardRow>
+                <TypeContainer>
+                    {
+                        pokemontype.map((item)=><Type type={item.type.name}>{item.type.name}</Type>)
+                    }
+                </TypeContainer>
+            <PokemonImg src={image} alt="Pokemon Image" />
+            </CardRow>
+        </CardContainer>
+    )
+}
 export default PokemonCard;
